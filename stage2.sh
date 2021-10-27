577f1a3d5980ba1de097d1b179976872edf7b0d7
Alexander Sosedkin <monk@unboiled.info>
2021-10-27 22:03:19 +0200

    stage2: configure gnumake without visible errors

--- stage2.sh
+++ stage2.sh
@@ -1,9 +1,40 @@
 #!/stage/1/bin/ash
 set -uex
 
-PATH=/stage/1/bin
+export PATH=/stage/1/bin
 
 echo 'Hi from stage 2!' | sed s/Hi/Hello/
 ls
+
+mkdir -p /dev; :>/null
+
+mkdir -p /stage/2/build/make
+cp -r /seed/2/src/make/* /stage/2/build/make/
+cd /stage/2/build/make
+
+TCC_ARGS='-g -nostdlib -nostdinc -std=c99 -D_XOPEN_SOURCE=700'
+INCLUDES='-I/seed/1/src/protomusl/src/include'
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/arch/x86_64"
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/stage0-generated/sed1"
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/stage0-generated/sed2"
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/arch/generic"
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/src/internal"
+INCLUDES="$INCLUDES -I/seed/1/src/protomusl/include"
+CFLAGS="$INCLUDES"
+LDFLAGS='-static -Wl,-whole-archive'
+LDFLAGS="$LDFLAGS /stage/1/lib/protomusl.a"
+LDFLAGS="$LDFLAGS /stage/1/obj/protomusl/crt/crt1.o"
+
+CC=/seed/1/bin/tcc \
+LD=/seed/1/bin/tcc \
+CPP="/seed/1/bin/tcc -E" \
+CFLAGS="$TCC_ARGS $CFLAGS" \
+CPPFLAGS="$TCC_ARGS $CFLAGS" \
+LDFLAGS="$LDFLAGS" \
+CONFIG_SHELL="/stage/1/bin/ash" \
+SHELL="/stage/1/bin/ash" \
+	ash ./configure \
+		--build x86_64-linux \
+		--disable-dependency-tracking
 
 exit 0
