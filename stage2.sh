783bf08b06ae7bac0b5495184866f3b481fc5579
Alexander Sosedkin <monk@unboiled.info>
2021-10-30 12:21:00 +0200

    stage2: create wrappers for tcc utilities

--- stage2.sh
+++ stage2.sh
@@ -5,11 +5,29 @@ export PATH=/stage/1/bin
 
 
 echo 'Hi from stage 2!' | sed s/Hi/Hello/
-
 mkdir -p /stage/2/bin
-
 mkdir -p /dev; :>/dev/null
 
+echo 'Creating wrappers for tcc'
+mkdir -p /stage/2/wrappers/tcc
+cd /stage/2/wrappers/tcc
+_TCC_ARGS='-g'
+_CPP_ARGS="$_TCC_ARGS -I/stage/1/include/protomusl"
+_LD_ARGS='-static -Wl,-whole-archive -lc'
+echo -e "#!/stage/1/bin/ash
+	if echo \"\$@\" | grep -w -- '-c'; then
+		exec /stage/1/bin/tcc $_TCC_ARGS \"\$@\"
+	else
+		exec /stage/1/bin/tcc $_TCC_ARGS $_LD_ARGS \"\$@\"
+	fi
+" > cc
+echo -e "#!/stage/1/bin/ash\nexec /stage/1/bin/tcc -E $_CPP_ARGS \"\$@\"" > cpp
+echo -e "#!/stage/1/bin/ash\nexec /stage/1/bin/tcc $_LD_ARGS \"\$@\"" > ld
+chmod +x cc cpp ld
+
+export PATH=/stage/1/bin:/stage/2/wrappers/tcc
+
+echo 'Building gnumake'
 rm -rf /stage/2/tmp/gnumake
 mkdir -p /stage/2/tmp/gnumake
 cp -r /seed/2/src/gnumake/* /stage/2/tmp/gnumake/
@@ -17,14 +35,8 @@ cd /stage/2/tmp/gnumake
 
 # this is part of stdlib, no idea how it's supposed to not clash
 rm src/getopt.h
-:> src/getopt.c
-:> src/getopt1.c
-:> lib/fnmatch.c
-:> lib/glob.c
-:> lib/xmalloc.c
-:> lib/error.c
-
-TCC_ARGS='-g -std=c99 -D_XOPEN_SOURCE=700'
+for f in src/getopt.c src/getopt1.c; do :> $f; done
+for f in lib/fnmatch.c lib/glob.c lib/xmalloc.c lib/error.c; do :> $f; done
 
 sed -i 's|/bin/sh|/stage/1/bin/ash|' src/job.c
 
@@ -32,12 +44,6 @@ ash ./configure \
 	--build x86_64-linux \
 	--disable-posix-spawn \
 	--disable-dependency-tracking \
-	CC=/stage/1/bin/tcc \
-	LD=/stage/1/bin/tcc \
-	CPP='/seed/1/bin/tcc -E'\
-	CFLAGS="$TCC_ARGS" \
-	CPPFLAGS="$TCC_ARGS -I/stage/1/include/protomusl" \
-	LDFLAGS='-static -Wl,-whole-archive -lc' \
 	CONFIG_SHELL='/stage/1/bin/ash' \
 	SHELL='/stage/1/bin/ash'
 # TODO: why the need to specify include dir in CPPFLAGS?
