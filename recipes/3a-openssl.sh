0ab5001e854893ba5a1db433e6a73671b8ca3bab
Alexander Sosedkin <monk@unboiled.info>
2021-11-22 22:41:18 +0100

    recipes/3a-openssl: increase reproducibility

--- recipes/3a-openssl.sh
+++ recipes/3a-openssl.sh
@@ -17,14 +17,17 @@ if [ -e /store/_2a0-ccache ]; then . /store/_2a0-ccache/wrap-available; fi
 echo "### $0: unpacking OpenSSL sources..."
 tar --strip-components=1 -xf /downloads/openssl-3.0.0.tar.gz
 
-echo "### $0: building OpenSSL..."
+echo "### $0: fixing up OpenSSL sources..."
 sed -i 's|/bin/sh|/store/2b2-busybox/bin/ash|' config
 sed -i 's|/usr/bin/env perl|/store/3a-perl/bin/perl|' Configure
+sed -i 's|date = .*|date = "Tue Jan 01 00:00:01 UTC 1970";|' \
+	util/mkbuildinf.pl
 
+echo "### $0: building OpenSSL..."
 CFLAGS='-I/store/2b1-clang/lib/clang/13.0.0/include'
 CFLAGS="$CFLAGS -I/store/2a6-linux-headers"
 CC=clang CFLAGS="$CFLAGS" ash config --prefix=/store/3a-openssl
 make -j $NPROC
 
 echo "### $0: installing OpenSSL..."
-make -j $NPROC install
+make -j $NPROC install_sw
