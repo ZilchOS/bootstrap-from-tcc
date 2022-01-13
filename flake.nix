6abd4dd70779eb5e7ad4bfa77c89c759a725e229
Alexander Sosedkin <monk@unboiled.info>
2022-01-13 08:41:45 +0100

    flake.nix: add hydraJobs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,15 @@
 {
   description = "bootstrap-from-tcc";
 
-  outputs = { self }: {
-    packages.x86_64-linux = (import ./default.nix);
-  };
+  outputs = { self }:
+    let
+      allPkgs = (import ./default.nix);
+    in
+      {
+        packages.x86_64-linux = allPkgs;
+
+        # TODO: expose only the most usable outputs
+        # TODO: solve fetching: https://discourse.nixos.org/t/17105
+        hydraJobs = builtins.mapAttrs (_: drv: { x86_64-linux = drv; }) allPkgs;
+      };
 }
