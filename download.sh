def948bc8424b98da52ecb405dc56c7ebc9b792f
Alexander Sosedkin <monk@unboiled.info>
2023-10-08 11:54:04 +0200

    download.sh: support parsing of #local/url/sha256 as well
    Builds-required: make=1 raw=1 nix=0

--- download.sh
+++ download.sh
@@ -3,6 +3,11 @@
 # Receives files as arguments, scans them for lines like
 # #> FETCH dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3
 # #>  FROM http://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+# #>    AS make.tar.gz
+# or
+#   #local = "/downloads/make.tar.gz";
+#   url = "http://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz";
+#   sha256 = "dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3";
 # downloads to ./downloads if file's not present there yet, verifies hash,
 # copies file over to $DESTDIR.
 
@@ -34,13 +39,16 @@ fetch() {
 	fi
 }
 
+REGEX_MAGIC='^#> '
+REGEX_FETCH='^#> FETCH'
+REGEX_FROM='^#>  FROM'
+REGEX_AS='^#>    AS'
+NIX_REGEX_FETCH='^[[:blank:]]*sha256[[:blank:]]*=[[:blank:]]*"(.*)";$'
+NIX_REGEX_FROM='^[[:blank:]]*url[[:blank:]]*=[[:blank:]]*"(.*)";$'
+NIX_REGEX_AS='^[[:blank:]]*#[[:blank:]]local[[:blank:]]*=[[:blank:]]*/downloads/(.*);$'
 process_commands_in() {
 	hash=''; url=''; filename=''
 	while read -r line; do
-		REGEX_MAGIC='^#> '
-		REGEX_FETCH='^#> FETCH'
-		REGEX_FROM='^#>  FROM'
-		REGEX_AS='^#>    AS'
 		if [[ "$line" =~ $REGEX_MAGIC ]]; then
 			if [[ "$line" =~ $REGEX_FETCH ]]; then
 				hash="${line##"#> FETCH"}"
@@ -52,6 +60,12 @@ process_commands_in() {
 				echo "### $0: malformed line '$line' in '$1'"
 				exit 2
 			fi
+		elif [[ "$line" =~ $NIX_REGEX_FETCH ]]; then
+		     hash=${BASH_REMATCH[1]}
+		elif [[ "$line" =~ $NIX_REGEX_FROM ]]; then
+		     url=${BASH_REMATCH[1]}
+		elif [[ "$line" =~ $NIX_REGEX_AS ]]; then
+			filename=${BASH_REMATCH[1]}
 		else
 			if [[ -n "$hash" && -n "$url" ]]; then
 				filename=${filename:-$(basename $url)}
